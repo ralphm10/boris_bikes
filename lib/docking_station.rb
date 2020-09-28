3e84d0d3ab3ff68c5b3b112b1e2d07fd68f3758b
Ralph Mallett <rmallett12@gmail.com>
2020-09-28 16:28:33 +0100

    test: raise error when calling release_bike on empty docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,8 @@ require_relative 'bike'
 class DockingStation
     attr_reader :bike
     def release_bike
-        Bike.new
+        raise 'no bikes available' unless @bike 
+        @bike
     end 
     def dock(bike)
         @bike = bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
-
-    it 'releases working bikes' do
-        bike = subject.release_bike
-        expect(bike).to be_working
+    
+    describe '#release_bike' do
+        it 'releases a bike' do
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq bike
+        end 
+        it 'raises an error if empty' do
+            expect { subject.release_bike }.to raise_error('no bikes available')
+        end 
     end 
 
     it 'docks something' do
